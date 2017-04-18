cbc0fcfa62bcbec9208181d7ad96d5df16668e64
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-04-18 14:12:26 -0300

    ajuste package json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./custom-express')
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const address = '127.0.0.1'
 const port = 8080
 
 app.set('io', io)
