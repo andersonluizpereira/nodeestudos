4e734ebac5958848d956306f71a806107544e277
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-04-18 13:35:43 -0300

    code

--- infra/connectionFactoryMSsql.js
+++ infra/connectionFactoryMSsql.js
@@ -6,18 +6,11 @@ function connectionFactoryMSsql(host, port, user, password, database) {
   console.log(database);
 
   let connection = mssql.createConnection({
-    host : host || '10.2.144.35',
-    user : user || 'WCH_PCK',
+    host : host || 'xxxxxxxx',
+    user : user || 'xxxxxxxxx',
     password : password || 'teste',
-    database : database || 'WCH_PCK'
+    database : database || 'teste'
   });
-
-  //  let connection = mssql.createConnection({
-  //   host :  '10.2.144.35',
-  //   user :  'WCH_PCK',
-  //   password :  '*5dPu(v',
-  //   database :  'MSPNP'
-  // });
 
   return connection
 }
