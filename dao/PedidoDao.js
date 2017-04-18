75d52f598db67eeaf71d26424efbee622ebde514
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-04-18 13:40:21 -0300

    code2

--- dao/PedidoDao.js
+++ dao/PedidoDao.js
@@ -3,7 +3,7 @@ function PedidoDao(connection) {
 }
 
 PedidoDao.prototype.listaTodos = function(callback) {
-  this._connection.query("select  situacao_logistica, numero_pedido_rls = count(numero_pedido_rls),cast( replace(sum(VL_TOTAL_CAPTADO_PEDIDO),',','.') as decimal(18,2)) as VL_TOTAL_CAPTADO_PEDIDO, cast( replace(sum(VL_TOTAL_FATURADO_PEDIDO),',','.')as decimal(18,2)) as VL_TOTAL_FATURADO_PEDIDO from tbPedidos_CAPTADOS group by situacao_logistica order by situacao_logistica", callback)
+  this._connection.query("select  **************", callback)
 }
 
 
